75c896afa8ce1fd9bf8a46c0b384c400e7fa5ceb
shineyounn <kjy040715@naver.com>
2025-04-09 17:34:47 +0900

    2025.4.9

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    grabCursor: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+    },
+});
